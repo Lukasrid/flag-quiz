fdefa8899e455df00e5be7eacf226697b817cefb
Lukasrid <lukas.ridstrom@gmail.com>
2022-12-12 22:13:29 +0000

    start defining some important constants

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const startButton = document.getElementById ('start');
+const nextButton = document.getElementById ('next');
+const flags = document.getElementById ('flag');
+const answer = document.getElementById ('answer-box')
