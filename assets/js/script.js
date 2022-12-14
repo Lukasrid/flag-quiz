62acacb1eb52caac6fb3cbfac5f797ee0387f769
Lukasrid <lukas.ridstrom@gmail.com>
2022-12-14 18:42:26 +0000

    add constants and hide unwanted elements when dom is loaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,31 @@
 const startButton = document.getElementById ('start');
 const nextButton = document.getElementById ('next');
 const flags = document.getElementById ('flag');
-const answer = document.getElementById ('answer-box')
+const answer = document.getElementById ('answer-box');
+const submit = document.getElementById ('submit');
+const score = document.getElementById ('score-count')
+const sbox = document.getElementById ('sbox')
+
+document.addEventListener("DOMContentLoaded", function () {
+  nextButton.style.visibility = 'hidden';
+  nextButton.style.display = 'none';
+  answer.style.visibility = 'hidden';
+  answer.style.display = 'none';
+  submit.style.visibility = 'hidden';
+  submit.style.display = 'none';
+  sbox.style.visibility = 'hidden';
+  sbox.style.display = 'none';
+});
+
+function startGame() {
+
+}
+
+function submitAnswer(){
+
+}
+
+function nextQuestion() {
+
+}
+
