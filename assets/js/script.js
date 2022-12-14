224ff7c20741cb0da6bd17e4a11c6bfcf9bc066f
Lukasrid <lukas.ridstrom@gmail.com>
2022-12-14 20:01:40 +0000

    hide elements before start of game and show game elements when start is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,47 @@
-const startButton = document.getElementById ('start');
-const nextButton = document.getElementById ('next');
-const flags = document.getElementById ('flag');
-const answer = document.getElementById ('answer-box');
-const submit = document.getElementById ('submit');
-const score = document.getElementById ('score-count')
-const sbox = document.getElementById ('sbox')
+const startButton = document.getElementById('start');
+const nextButton = document.getElementById('next');
+const flags = document.getElementById('flag');
+const answer = document.getElementById('answer-box');
+const submit = document.getElementById('submit');
+const score = document.getElementById('score-count')
+const sbox = document.getElementById('sbox')
 
 document.addEventListener("DOMContentLoaded", function () {
-  nextButton.style.visibility = 'hidden';
-  nextButton.style.display = 'none';
-  answer.style.visibility = 'hidden';
-  answer.style.display = 'none';
-  submit.style.visibility = 'hidden';
-  submit.style.display = 'none';
-  sbox.style.visibility = 'hidden';
-  sbox.style.display = 'none';
+    nextButton.style.visibility = 'hidden';
+    nextButton.style.display = 'none';
+    answer.style.visibility = 'hidden';
+    answer.style.display = 'none';
+    submit.style.visibility = 'hidden';
+    submit.style.display = 'none';
+    sbox.style.visibility = 'hidden';
+    sbox.style.display = 'none';
 });
 
-function startGame() {
+let shuffledQuestions, currentQuestionIndex
 
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+function startGame() {
+    startButton.style.visibility = 'hidden';
+    startButton.style.display = 'none';
+    nextButton.style.visibility = 'visible';
+    nextButton.style.display = 'inline';
+    answer.style.visibility = 'visible';
+    answer.style.display = 'inline';
+    submit.style.visibility = 'visible';
+    submit.style.display = 'inline';
+    sbox.style.visibility = 'visible';
+    sbox.style.display = 'inline';
 }
 
-function submitAnswer(){
+function submitAnswer() {
 
 }
 
 function nextQuestion() {
 
 }
-
